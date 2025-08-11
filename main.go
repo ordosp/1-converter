01b5da5acf1a6194672625b515fde20a1d1f8021
Pavel <ordosp@gmail.com>
2025-08-11 23:56:48 +0300

    Ex.4

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import "fmt"
 
+func userInput() float64 {
+	var amount float64
+	fmt.Scan(&amount)
+	return amount
+}
+
+func convertCurrency(amount float64, srcCurrency string, dstCurrency string) {
+
+}
+
 func main() {
 	const USD_EUR = 0.86
 	const USD_RUB = 79.26
 	const EUR_RUB = USD_RUB / USD_EUR
+	var amount float64
 
+	fmt.Print("Введите значение:")
+	amount = userInput()
+	fmt.Println(amount)
 	fmt.Println(100 * EUR_RUB)
 }
